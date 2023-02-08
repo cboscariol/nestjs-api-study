9360e3cb4d4abba07643901d6f1ed367aacfa188
camila <camila.boscariol@ab-inbev.com>
2023-02-08 15:03:54 -0300

    signin and signup logics

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,15 +24,31 @@ export class AuthService {
       return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
+        // check is the error is a prisma error
         if (error.code === 'P2002') {
+          //prisma error about duplications, garantee we don't have to users with same email
           throw new ForbiddenException('Credentials taken');
         }
       }
-      throw error;
+      throw error; // if its a regular error
     }
   }
 
-  signin() {
-    return 'I am signin in';
+  async signin(dto: AuthDto) {
+    //find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    //if user doesent exist trhow exeption
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    //if password dont match trhow exeption
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    delete user.hash;
+    return user;
   }
 }
